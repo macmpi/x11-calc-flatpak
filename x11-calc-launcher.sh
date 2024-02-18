ef0b9aa703388b0d3359f5de7ee7aeb12e445d48
macmpi <16296055+macmpi@users.noreply.github.com>
2024-02-18 17:23:07 +0100

    allow command-line options
    options can be passed & tested under CLI

--- x11-calc-launcher.sh
+++ x11-calc-launcher.sh
@@ -1,12 +1,33 @@
 #!/bin/sh
 
+# Simple launcher program to set default emulator model and
+# eventual run options.
+# Copyright(C) 2024 macmpi
+#
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
 MODEL=""
 OPTS=""
+CMD_OPTS=""
 
 _launch() {
 # shellcheck disable=SC1090  # intended include
 . "${XDG_CONFIG_HOME}"/x11-calc/x11-calc.conf
 [ -z "$MODEL" ] && exit 1
+[ -n "$CMD_OPTS" ] && OPTS="$CMD_OPTS"
 
 case $MODEL in
 	10c|11c|12c|15c|16c)
@@ -24,7 +45,7 @@ _setup() {
 nano "${XDG_CONFIG_HOME}"/x11-calc/x11-calc.conf
 }
 
-
+CMD_OPTS="$@"
 if ! [ -f "${XDG_CONFIG_HOME}"/x11-calc/x11-calc.conf ]; then
 	mkdir -p "${XDG_CONFIG_HOME}"/x11-calc
 	cat <<-EOF >"${XDG_CONFIG_HOME}"/x11-calc/x11-calc.conf
@@ -32,21 +53,22 @@ if ! [ -f "${XDG_CONFIG_HOME}"/x11-calc/x11-calc.conf ]; then
 		MODEL=35
 
 		# OPTS may contain options as one-liner string to specify:
-		# * preferred non-default save-state file path to be loaded
-		# (like sample prg presets from /app/share/x11-calc/prg/)
-		# * non-default .rom file path (-r prefix)
-		# * other debug options...
-		# more info at https://github.com/mike632t/x11-calc
+		# # preferred non-default save-state file path to be loaded
+		#  (like sample prg presets from /app/share/x11-calc/prg/)
+		# # non-default .rom file path (-r prefix)
+		# # other debug options...
+		# For more list of options, run from command-line:
+		# flatpak run io.github.mike632t.x11_calc --help
+		# To test OPTS line and diagnose errors, run from command-line:
+		# flatpak run io.github.mike632t.x11_calc OPTS
 		OPTS=""
 		
-		# To diagnose errors, run from command-line:
-		# flatpak run io.github.mike632t.x11_calc
 		# To call this setup again:
 		# flatpak run io.github.mike632t.x11_calc --setup
 		EOF
 fi
 
-case $1 in
+case $CMD_OPTS in
 	--setup)
 		_setup
 	;;
